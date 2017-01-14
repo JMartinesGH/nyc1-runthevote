ee58d4ce940bd184f9352b3c61507113d5241a40
Jeff Martines <martines.jeff@gmail.com>
2017-01-14 10:36:57 -0500

    stubbing out some pages, simple drag and drop for the ballot (unstyled), will use php to eventually tie disparate pieces together

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>Run the Vote</title>
 </head>
 <body>
+    <!-- quiz/intro -->
 
+    <!-- dashboard -->
+
+    <!-- ballot -->
+    <?php @include "partials/ballot.php"; ?>
+
+    <!-- results -->
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </body>
 </html>
---
+++ partials/ballot.php
@@ -0,0 +1,9 @@
+<?php  
+#ballot 
+?>
+
+<section>
+    <ul id="sortable">
+        <li></li>
+    </ul>
+</section>
