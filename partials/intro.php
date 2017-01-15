9b2bcf2df9effbf59e9d142dfbc3f8f7e43e5d32
Jeff Martines <martines.jeff@gmail.com>
2017-01-15 14:49:31 -0500

    intro copy, button fix

--- partials/intro.php
+++ partials/intro.php
@@ -5,7 +5,8 @@
     </div>
     <div class="hero-text">
       <h1>TruVote</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+      <p>Every citizen should feel empowered to vote their values regardless of partisan&#8212;  bias or worrying about complicated statistical outcomes. Use our interactive profiler tool to find the candidate that best matches your values — not party &#8212; lines — and cast your TruVote to see how everyone’s values really add up. 
+</p>
     </div>
     <div class="submit-wrapper">
       <a href="#madlib" id="get-started">Get Started</a href="madlib">
